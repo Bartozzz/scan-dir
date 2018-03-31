e75baded2679454c46d08e6aea873160c32b27a3
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-03-31 15:55:03 +0200

    Fix wrong path/filename on recursive load

--- src/index.js
+++ src/index.js
@@ -2,35 +2,33 @@
 import fs from "fs";
 import path from "path";
 
+type callback = (path: string, filename: string) => void;
+
 /**
  * Load files from a `directory` and execute a `callback` for each.
  *
- * @param   {string}    directory   Directory to load files from
+ * @param   {string}    dir         Directory to load files from
  * @param   {Function}  callback    Callback to execute on each file
  * @param   {bool}      recursive   Whether parse directories recursively
  * @return  {void}
  */
-function callDir(
-    directory: string,
-    callback: (path: string, filename: string) => void,
-    recursive: bool = false
-): void {
-    fs.readdirSync(path.resolve(process.cwd(), directory))
+function load(dir: string, callback: callback, recursive: bool = false): void {
+    fs.readdirSync(path.resolve(process.cwd(), dir))
         .filter((file: string) => !(/(^|\/)\.[^\/\.]/g).test(file))
         .forEach((file: string) => {
-            const filePath = path.join(directory, file);
+            const filePath = path.join(dir, file);
             const fileStat = fs.statSync(filePath);
 
             // Parses directories recursively if enabled
-            if (fileStat.isDirectory() && recursive) {
-                return callDir(filePath, callback, recursive);
+            if (fileStat.isDirectory()) {
+                return recursive && load(filePath, callback, recursive);
             }
 
-            // Execute callback onle for files with an extension
+            // Executes callback for files with extension
             if (filePath.indexOf(".") !== 0) {
                 return callback(filePath, file);
             }
         });
 }
 
-export default callDir;
+export default load;
